1a9f558c9ce50cbabd2e86f6a1ca8440fa03932e
Vlad Rindevich <vladrin@vaadin.com>
2023-06-24 00:47:21 +0300

    chore: add router with polyfill section to size-limit config

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -1,9 +1,18 @@
 module.exports = [
   {
+    name: 'Bare',
     path: ['index.js'],
     import: '*',
     modifyEsbuildConfig(cfg) {
       return { ...cfg, format: 'esm', target: 'esnext', packages: 'external' };
     },
   },
+  {
+    name: 'With Polyfill',
+    path: ['index.js'],
+    import: '*',
+    modifyEsbuildConfig(cfg) {
+      return { ...cfg, format: 'esm', target: 'esnext' };
+    },
+  }
 ];
