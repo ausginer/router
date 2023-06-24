787d8a33129551e15b6657c5dea2f5b24f06e854
Vlad Rindevich <vladrin@vaadin.com>
2023-06-24 22:13:49 +0300

    chore: simplify Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { readFile } from 'node:fs/promises';
 import { defineConfig } from 'vite';
-
-const tsconfig = await readFile(new URL('tsconfig.test.json', import.meta.url), 'utf8').then(JSON.parse);
 
 export default defineConfig({
   build: {
     target: 'esnext',
-  },
-  esbuild: {
-    tsconfigRaw: tsconfig,
   },
 });
