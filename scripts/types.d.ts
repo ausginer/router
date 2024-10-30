031ac6fa86d80a62aba62dc079f4bec81564992e
Vlad Rindevich <vladrin@vaadin.com>
2024-10-30 20:53:27 +0200

    chore: remove unnecessary typings

--- scripts/types.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export,import/unambiguous */
-declare module '@remcovaes/web-test-runner-vite-plugin' {
-  import type { TestRunnerPlugin } from '@web/test-runner';
-  import type { UserConfig } from 'vite';
-
-  export function vitePlugin(config?: UserConfig): () => TestRunnerPlugin;
-}
